6821ea985e304154fbfdaa966f795a5b68302dcc
Adoo2401 <abdullahrehmani230@gmail.com>
2024-05-19 19:50:50 +0800

    change backend url

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,4 +1,4 @@
-const baseURL='https://fine-puce-cobra-suit.cyclic.app/api/v1';
+const baseURL='https://incineration-backend.vercel.app/api/v1';
 export default baseURL;
 
 //https://fine-puce-cobra-suit.cyclic.app/api/v1
